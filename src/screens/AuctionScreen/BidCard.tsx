ceb2ee303da1f7a1e4cd68c923086b2f055acbb9
lzhabo <lidia.zhabo99@gmail.com>
2022-11-10 14:14:20 +0000

    added bids layout

--- src/screens/AuctionScreen/BidCard.tsx
+++ src/screens/AuctionScreen/BidCard.tsx
@@ -1,18 +1,99 @@
 import styled from "@emotion/styled";
 import React from "react";
+import BN from "@src/utils/BN";
+import { Column, Row } from "@src/components/Flex";
+import Text from "@src/components/Text";
+import { ReactComponent as More } from "@src/assets/icons/more.svg";
+import temp from "@src/assets/icons/temp.svg";
+import SizedBox from "@components/SizedBox";
+import Button from "@components/Button";
 
 interface IProps {
   status: string;
-  amount: string;
+  amount: BN;
   phase: string;
 }
 
 const Root = styled.div`
   display: flex;
   flex-direction: column;
+  background: #ffffff;
+  border: 2px solid #eeeeee;
+  border-radius: 12px;
+  padding: 16px;
 `;
 
-const BidCard: React.FC<IProps> = () => {
-  return <Root></Root>;
+const Status = styled.div<{ status: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 2px;
+  padding: 2px 8px;
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 20px;
+  ${({ status }) =>
+    (() => {
+      switch (status) {
+        case "bid":
+          return "background-color: #C1BFFF";
+        case "missed":
+          return "background-color: #FF938C";
+        case "reveal":
+          return "background-color: #FFDEA6";
+        case "winner":
+          return "background-color: #A5FFC9";
+        case "expired":
+          return "background-color: #EEEEEE";
+        default:
+          return "background-color: #EEEEEE";
+      }
+    })()}
+`;
+const NFT = styled.img`
+  width: 100%;
+`;
+const getBtnText = (status: string) => {
+  switch (status) {
+    case "bid":
+      return "Open Bid";
+    case "missed":
+      return "Open Bid";
+    case "reveal":
+      return "Claim NFT";
+    case "winner":
+      return "Refund Bid";
+    case "expired":
+      return "Refund Bid";
+    default:
+      return "Default";
+  }
+};
+const BidCard: React.FC<IProps> = ({ status, amount, phase }) => {
+  return (
+    <Root>
+      <Row justifyContent="space-between">
+        <Status status={status}>
+          {status.charAt(0).toUpperCase() + status.slice(1)}
+        </Status>
+        <More />
+      </Row>
+      <SizedBox height={18} />
+      <NFT src={temp} alt="temp" />
+      <SizedBox height={16} />
+      <Row justifyContent="space-between">
+        <Column>
+          <Text type="grey">My bid</Text>
+          <Text weight={700}>1,000.0000 WAVES</Text>
+        </Column>
+        <Column>
+          <Text type="grey">Next phase</Text>
+          <Text weight={700}>Claim your domain</Text>
+        </Column>
+      </Row>
+      <SizedBox height={16} />
+      <Button size="medium">{getBtnText(status)}</Button>
+    </Root>
+  );
 };
 export default BidCard;
--- src/screens/AuctionScreen/MyBids.tsx
+++ src/screens/AuctionScreen/MyBids.tsx
@@ -1,9 +1,11 @@
 import styled from "@emotion/styled";
 import React from "react";
 import Text from "@components/Text";
-import { Column, Row } from "@src/components/Flex";
+import { Row } from "@src/components/Flex";
 import Button from "@components/Button";
 import SizedBox from "@components/SizedBox";
+import BN from "@src/utils/BN";
+import BidCard from "@screens/AuctionScreen/BidCard";
 
 interface IProps {}
 
@@ -12,8 +14,26 @@ const Root = styled.div`
   flex-direction: column;
   width: 100%;
 `;
+const Bids = styled.div`
+  display: grid;
+  grid-gap: 24px;
+  grid-template-columns: 1fr;
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`;
+
 const MyBids: React.FC<IProps> = () => {
-  const bids = [{}];
+  const bids = [
+    { status: "bid", amount: new BN(10), phase: "12-12-2022" },
+    { status: "missed", amount: new BN(10), phase: "12-12-2022" },
+    { status: "reveal", amount: new BN(10), phase: "12-12-2022" },
+    { status: "winner", amount: new BN(10), phase: "12-12-2022" },
+    { status: "expired", amount: new BN(10), phase: "12-12-2022" },
+  ];
   return (
     <Root>
       <Row justifyContent="space-between">
@@ -35,7 +55,11 @@ const MyBids: React.FC<IProps> = () => {
         </Row>
       </Row>
       <SizedBox height={24} />
-      <Column></Column>
+      <Bids>
+        {bids.map((v, index) => (
+          <BidCard key={index} {...v} />
+        ))}
+      </Bids>
     </Root>
   );
 };
